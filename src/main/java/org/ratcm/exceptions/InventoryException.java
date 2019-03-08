e16a3109c920ec704283c76b19f2a5a2d03cdd53
raclet <raclet@irit.fr>
2019-03-08 17:26:04 +0100

    Initial commit

---
+++ src/main/java/org/ratcm/exceptions/InventoryException.java
@@ -0,0 +1,11 @@
+package org.ratcm.exceptions;
+
+public class InventoryException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public InventoryException(String msg) {
+		super(msg);
+	}
+
+}
